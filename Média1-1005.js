979ad39176823a06d448d60361af3beecb6e6f1e
Rodrigo de Oliveira <rodrigoleao.ci@gmail.com>
2021-02-18 14:39:23 -0300

    renomeando arquivos

--- Média1-1005.js
+++ Média1-1005.js
@@ -1,5 +1,7 @@
 const lines = prompt ('Digite nota A e B: ').split(' ')
 
+// lines.pop()  necessario no URI para excluir a ultima linha 
+
 const notas = (a , b) => (a*3.5 + b*7.5) / 11
 
 const resultado = lines.map(Number).reduce(notas)
